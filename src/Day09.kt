450d5721b2772693ad3d88e58903f235f1a90d09
Klemen Plestenjak <plestenjak.klemen@gmail.com>
2023-12-09 06:44:25 +0100

    Day 09

---
+++ src/Day09.kt
@@ -0,0 +1,46 @@
+fun main() {
+    val lines = get_input(9)
+    val nums = lines.map { it.split(" ").map { it.toInt() } }
+
+    fun getDiff(array: List<Int>): List<Int> {
+        val diff = IntArray(array.size - 1)
+
+        for (i in 0 until array.size - 1) {
+            diff[i] = array[i + 1] - array[i]
+        }
+        return diff.toList()
+    }
+
+    fun findNextNum(array: List<Int>): Pair<Int, Int> {
+        var diff = getDiff(array)
+
+        val diffs = ArrayList<List<Int>>()
+        diffs.add(array)
+
+        while (diff.count { it == 0 } != diff.size) {
+            diffs.add(diff)
+            diff = getDiff(diff)
+        }
+
+        var accNext = 0
+        var accPrev = 0
+        for (d in diffs.reversed()) {
+            accNext += d.last()
+            accPrev = d.first() - accPrev
+        }
+        return Pair(accNext, accPrev)
+    }
+
+    var nextNumSum = 0
+    var prevNumSum = 0
+    for (n in nums) {
+        val (next, prev) = findNextNum(n)
+
+        nextNumSum += next
+        prevNumSum += prev
+    }
+
+    println("Part 1: $nextNumSum")
+    println("Part 2: $prevNumSum")
+
+}
