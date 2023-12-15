b4c0256fd9119e10cd6025c3e303fdb429d2f289
Klemen Plestenjak <plestenjak.klemen@gmail.com>
2023-12-15 07:12:37 +0100

    Remove unnecessary hash map

--- src/Day15.kt
+++ src/Day15.kt
@@ -19,24 +19,28 @@ fun main() {
     }
     println("Part 1: $hashSum")
 
-    val boxes = Array<ArrayList<String>>(256){ ArrayList() }
-    val strengths = Array<HashMap<String, Int>>(256) { HashMap() }
+    data class Lens(val label: String, var strength: Int = 0)
+
+    val boxes = Array<ArrayList<Lens>>(256){ ArrayList() }
+
 
     for (seq in initSequence) {
         val op = if (seq.contains('-')) '-' else '='
         val label = seq.split(op)[0]
         val strength = seq.substring(label.length + 1)
         val boxIdx = hashString(label)
+        val box = boxes[boxIdx]
 
         when (op) {
             '-' -> {
-                boxes[boxIdx].remove(label)
+                box.removeIf { it.label == label }
             }
             '=' -> {
-                if (label !in boxes[boxIdx]) {
-                    boxes[boxIdx].add(label)
+                if (!box.any { it.label == label }) {
+                    boxes[boxIdx].add(Lens(label))
                 }
-                strengths[boxIdx][label] = strength.toInt()
+                val lens = boxes[boxIdx].find { it.label == label }
+                lens!!.strength = strength.toInt()
             }
             else -> assert(false)
         }
@@ -49,7 +53,7 @@ fun main() {
         //    println("$boxIdx: $box")
         for (lensIdx in box.indices) {
             val lens = box[lensIdx]
-            focusingPower += (boxIdx + 1) * (lensIdx + 1) * strengths[boxIdx][lens]!!
+            focusingPower += (boxIdx + 1) * (lensIdx + 1) * lens.strength
         }
     }
     println("Part 2: $focusingPower")
