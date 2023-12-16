f955d7037199b4b6dfc8e48eb9234f9ea2e98679
Klemen Plestenjak <plestenjak.klemen@gmail.com>
2023-12-16 06:34:53 +0100

    Day 16

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,70 +1,83 @@
+import kotlin.math.max
+
 fun main() {
-    val input = get_input(0).map { it.toCharArray() }
+    val input = get_input(16).map { it.toCharArray() }
 
     val sizeX = input[0].size
     val sizeY = input.size
 
     data class Beam(var x: Int, var y: Int, var dX: Int, var dY: Int)
 
-    val energized = HashSet<Pair<Int, Int>>()
+    fun getEnergized(queue: ArrayDeque<Beam>): HashSet<Pair<Int, Int>> {
+        val energized = HashSet<Pair<Int, Int>>()
+        val visited = HashSet<Beam>()
+
+        while (queue.isNotEmpty()) {
+            val beam = queue.removeFirst()
+
+            beam.x += beam.dX
+            beam.y += beam.dY
+
+            if (beam.x !in 0..<sizeX || beam.y !in 0..<sizeY)
+                continue
+            if (beam in visited) continue
+            visited.add(beam)
+
+            energized.add(Pair(beam.x, beam.y))
+
+            val c = input[beam.y][beam.x]
+            when (c) {
+                '.' -> {
+                    queue.addLast(beam)
+                }
+                '/' -> {
+                    queue.addLast(Beam(beam.x, beam.y, -beam.dY, -beam.dX))
+                }
+                '\\' -> {
+                    queue.addLast(Beam(beam.x, beam.y, beam.dY,  beam.dX))
+                }
+                '|' -> {
+                    if (beam.dX == 0) {
+                        queue.addLast(beam)
+                    } else {
+                        queue.addLast(Beam(beam.x, beam.y, 0, 1))
+                        queue.addLast(Beam(beam.x, beam.y, 0, -1))
+                    }
+                }
+                '-' -> {
+                    if (beam.dY == 0) {
+                        queue.addLast(beam)
+                    } else {
+                        queue.addLast(Beam(beam.x, beam.y, 1, 0))
+                        queue.addLast(Beam(beam.x, beam.y, -1, 0))
+                    }
+                }
+            }
+        }
+        return energized
+    }
+
+
     val q = ArrayDeque<Beam>()
     q.add(Beam(-1, 0, 1, 0))
-    energized.add(Pair(0, 0))
-    val visited = HashSet<Beam>()
+    println("Part 1: ${getEnergized(q).size}")
 
-    while (q.isNotEmpty()) {
-        val beam = q.removeFirst()
+    var maxEnergized = 0
 
-        beam.x += beam.dX
-        beam.y += beam.dY
-
-        if (beam.x !in 0..<sizeX || beam.y !in 0..<sizeY)
-            continue
-        if (beam in visited) continue
-        visited.add(beam)
-
-        energized.add(Pair(beam.x, beam.y))
-
-        val c = input[beam.y][beam.x]
-        when (c) {
-            '.' -> {
-                q.addLast(beam)
-            }
-            '/' -> {
-                q.addLast(Beam(beam.x, beam.y, -beam.dY, -beam.dX))
-            }
-            '\\' -> {
-                q.addLast(Beam(beam.x, beam.y, beam.dY,  beam.dX))
-            }
-            '|' -> {
-                if (beam.dX == 0) {
-                    q.addLast(beam)
-                } else {
-                    q.addLast(Beam(beam.x, beam.y, 0, 1))
-                    q.addLast(Beam(beam.x, beam.y, 0, -1))
-                }
-            }
-            '-' -> {
-                if (beam.dY == 0) {
-                    q.addLast(beam)
-                } else {
-                    q.addLast(Beam(beam.x, beam.y, 1, 0))
-                    q.addLast(Beam(beam.x, beam.y, -1, 0))
-                }
-            }
-        }
-    }
-    for (y in input.indices) {
-        val line = input[y]
-        for (x in line.indices) {
-            if (Pair(x, y) in energized)
-                print("#")
-            else
-                print(line[x])
-        }
-        println()
+    fun simulateBean(beam: Beam) {
+        q.clear()
+        q.add(beam)
+        maxEnergized = max(maxEnergized, getEnergized(q).size)
     }
 
+    for (x in 0..sizeX) {
+        simulateBean(Beam(x, -1, 0, 1))
+        simulateBean(Beam(x, sizeY, 0, -1))
+    }
+    for (y in 0..sizeY) {
+        simulateBean(Beam(-1, y, 1, 0))
+        simulateBean(Beam(sizeX, y, -1, 0))
+    }
 
-    println("Part 1: ${energized.size}")
+    println("Part 2: $maxEnergized")
 }
